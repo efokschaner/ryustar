6457c220cff600a25658998825d7fc6f3dd10df3
efokschaner <efokschaner@riotgames.com>
2018-01-12 19:06:11 -0800

    Increase counter shards to 128 based on load test results

--- app-engine/server/config.py
+++ app-engine/server/config.py
@@ -9,7 +9,7 @@ IS_PUBLIC_ENVIRONMENT = os.environ.get('SERVER_SOFTWARE', '').startswith('Google
 
 class PersistentConfig(FancyModel):
     updated_timestamp = ndb.DateTimeProperty(auto_now=True)
-    initial_num_shards_for_vote_counters = ndb.IntegerProperty(default=50)
+    initial_num_shards_for_vote_counters = ndb.IntegerProperty(default=128)
     websocket_url = ndb.StringProperty(
         default='wss://gke.ryustar.io/websocket' if IS_PUBLIC_ENVIRONMENT else 'ws://gke.ryustar.invalid/websocket')
     # These defaults are the public test keys from https://developers.google.com/recaptcha/docs/faq

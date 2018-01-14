a2a54dcc5ce9ba5b231ae3b3fb34a8055247eef7
kpinkstaff <keri.pinkstaff@gmail.com>
2018-01-13 19:59:59 -0800

    WIP UI stuff

---
+++ client/src/components/MarioBlock.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="grid">
+    <div class="cell-top-left"></div>
+    <div class="cell-top-middle"></div>
+    <div class="cell-top-right"></div>
+    <div class="cell-middle-left"></div>
+    <div class="cell-middle-middle">
+      <slot></slot>
+    </div>
+    <div class="cell-middle-right"></div>
+    <div class="cell-bottom-left"></div>
+    <div class="cell-bottom-middle"></div>
+    <div class="cell-bottom-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MarioBlock',
+  components: {},
+  props: [
+    'state'
+  ],
+  created () {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+$light-edge: rgb(237, 115, 28);
+$dark-edge: rgb(0, 0, 11);
+$center: rgb(253, 206, 127);
+$edge-width: 6px;
+
+.grid {
+  display: inline-grid;
+  grid-template-rows: $edge-width 1fr $edge-width;
+  grid-template-columns: $edge-width 1fr $edge-width;
+  justify-content: start;
+  align-content: start;
+  justify-items: stretch;
+  align-items: stretch;
+}
+.cell-top-left {
+    
+}
+.cell-top-middle {
+    background-color: $light-edge;
+}
+.cell-top-right {
+    
+}
+.cell-middle-left {
+    background-color: $light-edge;
+}
+.cell-middle-middle {
+    background-color: $center;
+}
+.cell-middle-right {
+    background-color: $dark-edge;
+}
+.cell-bottom-left {
+    background-color: $dark-edge;
+}
+.cell-bottom-middle {
+    background-color: $dark-edge;
+}
+.cell-bottom-right {
+    background-color: $dark-edge;
+}
+</style>
--- client/src/components/Voting.vue
+++ client/src/components/Voting.vue
@@ -1,51 +1,58 @@
 <template>
-  <div class="voting">
-    <img src="../assets/ryukahr_logo.png" class="ryulogo">
-    <div v-if="noLevelInProgress">
-      <h2>No Level</h2>
-      <p>Looks like Ryukahr is not playing a level at the moment</p>
-    </div>
-    <div v-else-if="currentLevel">
-      <div v-if="captchaConsentResolveCallback">
-        <vue-recaptcha
-            style="display: none"
-            ref="invisibleRecaptcha"
-            badge="inline"
-            size="invisible"
-            theme="dark"
-            :sitekey="recaptchaSiteKey"
-            @verify="onCaptchaVerify"
-            @expired="onCaptchaExpired">
-        </vue-recaptcha>
-        <h3>This site uses Invisible ReCAPTCHA</h3>
-        <p>To make sure that votes aren't being submitted by robots,
-          this page uses Google's Invisible reCAPTCHA system for verifying that you are not a robot,
-          or not a robot lacking human intelligence at least!
-          reCAPTCHA works by "collecting hardware and software information,
-          such as device and application data and the results of integrity checks,
-          and sending that data to Google for analysis".
-          Your use of Invisible reCAPTCHA is subject to Google's
-          <a href="https://www.google.com/policies/privacy/">Privacy Policy</a> and
-          <a href="https://www.google.com/policies/terms/">Terms of Use</a>
-          Do you accept these terms, to continue with your vote?
-        </p>
-        <button @click="onRefusedCaptcha()">No thanks! I'll just spectate.</button>
-        <button @click="onConsentToCaptcha()">I accept these terms and wish to vote!</button>
+  <div class="voting-scene">
+    <div class="voting-ui">
+      <mario-block class="logo">
+        <!-- Hacky Kerning fix incoming -->
+        <span class="logo-text">RyuS<span style="letter-spacing: -4px;">t</span>ar.io</span>
+      </mario-block>
+      <div v-if="noLevelInProgress">
+        <h2>No Level</h2>
+        <p>Looks like Ryukahr is not playing a level at the moment</p>
       </div>
-      <div v-else>
-        <h2 class="current-level-header">Vote on the level: {{currentLevel.name_ish }}</h2>
-        <div class="votes-container">
-          <vote-item choice="star" @chosen="submitVote('star')">
-            <img src="../assets/star_level_256.png"/>
-          </vote-item>
-          <vote-item choice="garbage" @chosen="submitVote('garbage')">
-            <img src="../assets/trash_button_256.png"/>
-          </vote-item>
+      <div v-else-if="currentLevel">
+        <div v-if="captchaConsentResolveCallback">
+          <vue-recaptcha
+              style="display: none"
+              ref="invisibleRecaptcha"
+              badge="inline"
+              size="invisible"
+              theme="dark"
+              :sitekey="recaptchaSiteKey"
+              @verify="onCaptchaVerify"
+              @expired="onCaptchaExpired">
+          </vue-recaptcha>
+          <h3>This site uses Invisible ReCAPTCHA</h3>
+          <p>To make sure that votes aren't being submitted by robots,
+            this page uses Google's Invisible reCAPTCHA system for verifying that you are not a robot &mdash;
+            or not a robot lacking human intelligence at least! &mdash;
+            reCAPTCHA works by "collecting hardware and software information,
+            such as device and application data and the results of integrity checks,
+            and sending that data to Google for analysis".
+            Your use of Invisible reCAPTCHA is subject to Google's
+            <a href="https://www.google.com/policies/privacy/">Privacy Policy</a> and
+            <a href="https://www.google.com/policies/terms/">Terms of Use</a>.</p>
+            <p>Do you accept these terms?</p>
+          <button @click="onRefusedCaptcha()">No thanks! I'll just spectate...</button>
+          <button @click="onConsentToCaptcha()">I accept these terms and wish to vote!</button>
+        </div>
+        <div v-else>
+          <h4>Now voting on:</h4>
+          <h2>{{ currentLevel.name_ish }}</h2>
+          <div class="votes-container">
+            <vote-item choice="star" @chosen="submitVote('star')">
+              <img src="../assets/star_level_256.png"/>
+            </vote-item>
+            <vote-item choice="garbage" @chosen="submitVote('garbage')">
+              <img src="../assets/trash_button_256.png"/>
+            </vote-item>
+          </div>
         </div>
       </div>
+      <div v-else>
+        <h2>Fetching level info ...</h2>
+      </div>
     </div>
-    <div v-else>
-      <h2>Fetching level info ...</h2>
+    <div class="ground-texture">
     </div>
   </div>
 </template>
@@ -53,10 +60,15 @@
 <script>
 import { mapActions, mapGetters, mapState } from 'vuex'
 import VueRecaptcha from 'vue-recaptcha'
+import MarioBlock from './MarioBlock'
 import VoteItem from './VoteItem'
 export default {
   name: 'Voting',
-  components: { VueRecaptcha, VoteItem },
+  components: {
+    MarioBlock,
+    VueRecaptcha,
+    VoteItem
+  },
   created () {
     return Promise.all([
       this.fetchCurrentLevel(),
@@ -149,38 +161,51 @@ export default {
   }
 }
 </script>
-<style scoped>
-@import 'https://fonts.googleapis.com/css?family=Bowlby+One+SC';
-.voting {
-  height: 100vh;
-  background-image: url("../assets/wallpaper.png");
-  background-repeat:no-repeat;
-  background-size:cover;
+<style lang="scss" scoped>
+
+$text-color: rgb(0, 0, 11);
+$text-highlight-color: rgb(255, 182, 55);
+
+.voting-scene {
+  min-height: 100vh;
+  background: linear-gradient(rgb(63, 97, 233), rgba(63, 97, 233, 0.6));
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  color: $text-color;
+}
+.voting-ui {
+  padding: 30px;
+  max-width: 860px;
+}
+.logo {
+  text-align: left;
+}
+.logo-text {
+  font-family: 'Bou College', Helvetica, sans-serif;
+  font-size: 300%;
+  padding: 0 10px;
+}
+h1, h2, h3, h4, h5, h6 {
   font-family: 'Bowlby One SC', Helvetica, sans-serif;
-  text-align: center;
-}
-h1, h2 {
-  text-shadow: 2px 2px 5px red;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+  letter-spacing: 0.09em;
+  color: $text-highlight-color;
+  text-shadow: 3px 3px 3px $text-color;
 }
 a {
-  color: #be3535;
-}
-.current-level-header {
-  font-size: 200%;
-  color: "#000000";
+  color: $text-highlight-color;
 }
 .votes-container{
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: baseline;
+}
+.ground-texture{
+  height: 57px;
+  width: 100%;
+  background-image: url('../assets/floor.png');
+  background-repeat: repeat-x;
 }
 </style>
